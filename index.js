08591d16248c50320fe1ce74f2f06e1d22ef6c07
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-11 04:24:17 -0400

    add search functionality to index.html

---
+++ index.js
@@ -0,0 +1,24 @@
+const p = document.querySelector("p");
+const url = new URL ("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=8d980e10b733d1de5c4782ac5c258318")
+const city = document.querySelector("input");
+
+function setCity() {
+    url.searchParams.set('q', city.value);
+    if (city.value == "") {
+        p.innerText = "please enter a city"
+    } else {
+    getData();
+    };
+}
+
+async function getData() {
+    const response = await fetch(url, {mode: 'cors'});
+    const wData = await response.json();
+    if (wData.cod == "404") {
+        p.innerText = wData.message;
+    } else {
+    p.innerText = wData.weather[0].main;
+    };
+}
+
+getData();
